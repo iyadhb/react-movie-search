8cadfde4835927b2d0214f87e0a028e750ff812a
Iyad <iyadhb@gmail.com>
2020-10-02 16:47:15 +0100

    created call to API

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 export default function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log('submiting');
+
+    const query = 'jurassic Park';
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=9731893ef558e99434468e0deb477600&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={searchMovies}>
       <label className='label' htmlFor='query'>
         Movie Name
       </label>
